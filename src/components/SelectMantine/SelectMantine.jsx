cb156bd50cef4d240543b600c15a8bbaf0a9cebc
vitalyvitmens <otm_@tut.by>
2024-02-26 22:24:26 +0300

    feat(.prettierrc.json): добавляет prettier

--- src/components/SelectMantine/SelectMantine.jsx
+++ src/components/SelectMantine/SelectMantine.jsx
@@ -2,25 +2,30 @@ import { forwardRef } from 'react'
 import { Select } from '@mantine/core'
 import '@mantine/core/styles.css'
 
-export const SelectMantine = forwardRef(({ label, data, type, id, name, value, defaultValue, onChange, children }, ref) => {
-
-  return (
-    <Select
-    label={label} 
-    data={data}
-    type={type} 
-    id={id} 
-    value={value}
-    name={name} 
-    defaultValue={defaultValue}
-    onChange={onChange}
-    ref={ref}>
-      {children}
-      {/* <option value="">Без сортировки</option>
+export const SelectMantine = forwardRef(
+  (
+    { label, data, type, id, name, value, defaultValue, onChange, children },
+    ref,
+  ) => {
+    return (
+      <Select
+        label={label}
+        data={data}
+        type={type}
+        id={id}
+        value={value}
+        name={name}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        ref={ref}
+      >
+        {children}
+        {/* <option value="">Без сортировки</option>
       <option value="ASC">По возрастанию</option>
       <option value="DESC">По убыванию</option> */}
-    </Select>
- )
-})
+      </Select>
+    )
+  },
+)
 
 SelectMantine.displayName = 'SelectMantine'
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -8,195 +8,195 @@ import { useFetchCategory } from '../../hooks/useFetchCategory.jsx'
 import styles from './Category.module.css'
 
 export const Category = () => {
-	const [searchParams, setSearchParams] = useSearchParams()
-	const sort = searchParams.get('sort')
-	const [query, setQuery] = useState('')
-	const [pageNumber, setPageNumber] = useState(1)
-	const navigate = useNavigate()
-	const [isPending, startTransition] = useTransition()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const sort = searchParams.get('sort')
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const navigate = useNavigate()
+  const [isPending, startTransition] = useTransition()
 
-	const { loading, error, categories, hasMore, category, id } =
-		useFetchCategory(query, pageNumber)
+  const { loading, error, categories, hasMore, category, id } =
+    useFetchCategory(query, pageNumber)
 
-	const lastNodeObserver = useRef()
-	const lastNodeRef = useCallback(
-		(node) => {
-			if (loading) return
+  const lastNodeObserver = useRef()
+  const lastNodeRef = useCallback(
+    (node) => {
+      if (loading) return
 
-			if (lastNodeObserver.current) {
-				lastNodeObserver.current.disconnect()
-			}
+      if (lastNodeObserver.current) {
+        lastNodeObserver.current.disconnect()
+      }
 
-			lastNodeObserver.current = new IntersectionObserver((entries) => {
-				if (entries[0].isIntersecting && hasMore) {
-					setPageNumber((prevState) => prevState + 1)
-				}
-			})
+      lastNodeObserver.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevState) => prevState + 1)
+        }
+      })
 
-			if (node) {
-				lastNodeObserver.current.observe(node)
-			}
-		},
-		[hasMore, loading]
-	)
+      if (node) {
+        lastNodeObserver.current.observe(node)
+      }
+    },
+    [hasMore, loading],
+  )
 
-	const firstNodeObserver = useRef()
-	const firstNodeRef = useCallback(
-		(node) => {
-			if (loading) return
+  const firstNodeObserver = useRef()
+  const firstNodeRef = useCallback(
+    (node) => {
+      if (loading) return
 
-			if (firstNodeObserver.current) {
-				firstNodeObserver.current.disconnect()
-			}
+      if (firstNodeObserver.current) {
+        firstNodeObserver.current.disconnect()
+      }
 
-			firstNodeObserver.current = new IntersectionObserver((entries) => {
-				if (entries[0].isIntersecting) {
-					setPageNumber(1)
-				}
-			})
+      firstNodeObserver.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPageNumber(1)
+        }
+      })
 
-			if (node) {
-				firstNodeObserver.current.observe(node)
-			}
-		},
-		[loading]
-	)
+      if (node) {
+        firstNodeObserver.current.observe(node)
+      }
+    },
+    [loading],
+  )
 
-	const sortByCreated = (array, order) => {
-		const copy = [...(array || [])]
-		copy.sort((a, b) => {
-			const dateA = Date.parse(a.created)
-			const dateB = Date.parse(b.created)
-			if (order === 'ASC') {
-				return dateA - dateB
-			} else if (order === 'DESC') {
-				return dateB - dateA
-			} else {
-				return 0
-			}
-		})
-		return copy
-	}
+  const sortByCreated = (array, order) => {
+    const copy = [...(array || [])]
+    copy.sort((a, b) => {
+      const dateA = Date.parse(a.created)
+      const dateB = Date.parse(b.created)
+      if (order === 'ASC') {
+        return dateA - dateB
+      } else if (order === 'DESC') {
+        return dateB - dateA
+      } else {
+        return 0
+      }
+    })
+    return copy
+  }
 
-	const handleChangeSort = (event) => {
-		const value = event.target.value
-		startTransition(() => {
-			setQuery(id)
-			setPageNumber(1)
-			setSearchParams({ sort: value })
-		})
-	}
+  const handleChangeSort = (event) => {
+    const value = event.target.value
+    startTransition(() => {
+      setQuery(id)
+      setPageNumber(1)
+      setSearchParams({ sort: value })
+    })
+  }
 
-	const handlerScrollUp = () => {
-		window.scrollTo(0, 0)
-		startTransition(() => {})
-	}
+  const handlerScrollUp = () => {
+    window.scrollTo(0, 0)
+    startTransition(() => {})
+  }
 
-	const isCharacter = category === 'characters'
-	const imgStyle = isCharacter && {
-		width: '40px',
-		height: '40px',
-		margin: '1px 20px 1px 10px',
-		transform: 'translateY(15px)',
-		border: '1px solid #084949',
-		borderRadius: '50%',
-		boxShadow: '-4px -2px 10px black',
-	}
+  const isCharacter = category === 'characters'
+  const imgStyle = isCharacter && {
+    width: '40px',
+    height: '40px',
+    margin: '1px 20px 1px 10px',
+    transform: 'translateY(15px)',
+    border: '1px solid #084949',
+    borderRadius: '50%',
+    boxShadow: '-4px -2px 10px black',
+  }
 
-	if (category && !['characters', 'locations', 'episodes'].includes(category)) {
-		return <NotFound />
-	}
+  if (category && !['characters', 'locations', 'episodes'].includes(category)) {
+    return <NotFound />
+  }
 
-	return (
-		<div className={styles.Category}>
-			<form>
-				<Component
-					component={SelectMantine}
-					label="Сортировать по дате создания:"
-					data={["Без сортировки", "По возрастанию", "По убыванию"]}
-					type="select"
-					id="select"
-					name="select"
-					// value={sort || ''}
-					defaultValue="Без сортировки"
-					onChange={handleChangeSort}
-				/>
-			</form>
-			<ol style={{ marginLeft: '2rem' }}>
-				{sortByCreated(categories, sort).map((item, index) => {
-					if (categories.length - 17 === index + 1) {
-						return (
-							<li ref={lastNodeRef} key={index}>
-								<Link to={`/${category}/${item.id}`}>
-									{isCharacter && (
-										<img style={imgStyle} src={item.image} alt={item.name} />
-									)}
-									{item.name}
-								</Link>
-							</li>
-						)
-					} else if (index === 0) {
-						return (
-							<li ref={firstNodeRef} key={index}>
-								<Link to={`/${category}/${item.id}`}>
-									{isCharacter && (
-										<img style={imgStyle} src={item.image} alt={item.name} />
-									)}
-									{item.name}
-								</Link>
-							</li>
-						)
-					} else {
-						return (
-							<li ref={lastNodeRef} key={index}>
-								<Link to={`/${category}/${item.id}`}>
-									{isCharacter && (
-										<img style={imgStyle} src={item.image} alt={item.name} />
-									)}
-									{item.name}
-								</Link>
-							</li>
-						)
-					}
-				})}
-				{loading && hasMore && (
-					<div style={{ color: 'red', fontSize: '2rem' }}>Loading...</div>
-				)}
-				{!loading && !hasMore && (
-					<div
-						style={{
-							display: 'flex',
-							alignItems: 'end',
-							color: 'blue',
-							fontSize: '2rem',
-							fontWeight: '600',
-							textShadow: '-2px 2px 2px black',
-						}}
-					>
-						Конец списка
-						<Component
-							component={BtnMantine}
-							style={{border: '1px solid black', marginLeft: '5px'}}
-							title="В начало списка"
-							variant="gradient"
+  return (
+    <div className={styles.Category}>
+      <form>
+        <Component
+          component={SelectMantine}
+          label="Сортировать по дате создания:"
+          data={['Без сортировки', 'По возрастанию', 'По убыванию']}
+          type="select"
+          id="select"
+          name="select"
+          // value={sort || ''}
+          defaultValue="Без сортировки"
+          onChange={handleChangeSort}
+        />
+      </form>
+      <ol style={{ marginLeft: '2rem' }}>
+        {sortByCreated(categories, sort).map((item, index) => {
+          if (categories.length - 17 === index + 1) {
+            return (
+              <li ref={lastNodeRef} key={index}>
+                <Link to={`/${category}/${item.id}`}>
+                  {isCharacter && (
+                    <img style={imgStyle} src={item.image} alt={item.name} />
+                  )}
+                  {item.name}
+                </Link>
+              </li>
+            )
+          } else if (index === 0) {
+            return (
+              <li ref={firstNodeRef} key={index}>
+                <Link to={`/${category}/${item.id}`}>
+                  {isCharacter && (
+                    <img style={imgStyle} src={item.image} alt={item.name} />
+                  )}
+                  {item.name}
+                </Link>
+              </li>
+            )
+          } else {
+            return (
+              <li ref={lastNodeRef} key={index}>
+                <Link to={`/${category}/${item.id}`}>
+                  {isCharacter && (
+                    <img style={imgStyle} src={item.image} alt={item.name} />
+                  )}
+                  {item.name}
+                </Link>
+              </li>
+            )
+          }
+        })}
+        {loading && hasMore && (
+          <div style={{ color: 'red', fontSize: '2rem' }}>Loading...</div>
+        )}
+        {!loading && !hasMore && (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'end',
+              color: 'blue',
+              fontSize: '2rem',
+              fontWeight: '600',
+              textShadow: '-2px 2px 2px black',
+            }}
+          >
+            Конец списка
+            <Component
+              component={BtnMantine}
+              style={{ border: '1px solid black', marginLeft: '5px' }}
+              title="В начало списка"
+              variant="gradient"
               gradient={{ from: 'red', to: 'violet', deg: 64 }}
-							disabled={isPending}
-							onClick={handlerScrollUp}
-						/>
-						<Component
-							component={BtnMantine}
-							style={{border: '1px solid black', marginLeft: '5px'}}
-							title="На главную"
-							variant="gradient"
+              disabled={isPending}
+              onClick={handlerScrollUp}
+            />
+            <Component
+              component={BtnMantine}
+              style={{ border: '1px solid black', marginLeft: '5px' }}
+              title="На главную"
+              variant="gradient"
               gradient={{ from: 'red', to: 'violet', deg: 64 }}
-							disabled={isPending}
-							onClick={() => navigate('/')}
-						/>
-						{isPending && <div>Загрузка...</div>}
-					</div>
-				)}
-				{error && <div>Error</div>}
-			</ol>
-		</div>
-	)
+              disabled={isPending}
+              onClick={() => navigate('/')}
+            />
+            {isPending && <div>Загрузка...</div>}
+          </div>
+        )}
+        {error && <div>Error</div>}
+      </ol>
+    </div>
+  )
 }
