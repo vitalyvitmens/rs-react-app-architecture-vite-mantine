81ed23db45c696471a943384f95c1967f1c90739
vitalyvitmens <otm_@tut.by>
2024-02-27 12:59:46 +0300

    chore(vite.config.js): добавляет поле base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rs-react-app-architecture-vite-mantine/',
   plugins: [react()],
 })
